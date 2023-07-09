f6e545537b5594c71d102813f3e49be9c8852c24
Amrit Singh <72596236+Green-Dev-il@users.noreply.github.com>
2023-07-09 05:45:15 +0530

    Add files via upload

---
+++ 22 F-String.py
@@ -0,0 +1,14 @@
+# F-String in Python
+
+greeting = "Hello!"
+name = "Amrit"
+age = 16
+location = "Uttrakhand"
+
+print(f"{greeting} my name is {name}. I'm {age} and I live in {location}")
+
+# So, yeah this is f-string. Let's see another example
+
+earning = 49.83754
+print(f"Hey! I earned {earning} today.")
+print(f"Hey! I earned {earning:.2f} today.") # using this cammand you can print only two dicemal points.
