24c7633f671353d268ca83cf557185503547d7f8
Amrit Singh <72596236+Green-Dev-il@users.noreply.github.com>
2024-02-11 08:42:37 +0530

    Update 13 For Loops.py

--- 13 For Loops.py
+++ 13 For Loops.py
@@ -24,7 +24,7 @@ for k in z:
     print(k)
 # It start with a given integer (X) and stop at given integer (Y-1)
 print("\n")
-# 3. Step: range
-z = range(1, 11, 2)
+# 3. Step: range[(start: int), (stop: int), (step: int)]
+z = range(1, 11, 2) # in this it will print value after a jump
 for k in z:
-    print(k)
+    print(k) # in this case ther is a step of 2 it means 2-1=1 it will skip one(only) interger between two adjicant intergers like "1234" then "13" it skipped 2 and 4 i.e "1 [2] 3 [4]". Now lt's sppose there a step of 3 means 3-1=2 then it will skip two(only) interger between two adjicant intergers like "1234" then "14" it skipped 2 and 3 i.e "1 [2] [3] 4"
