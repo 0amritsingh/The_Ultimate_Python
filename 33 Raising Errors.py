8c10f25e85c53f8965874efb2fdaa214ec1bca10
Amrit Singh <amritsingh72007@gmail.com>
2024-05-19 10:28:36 +0530

    updated file

--- 33 Raising Errors.py
+++ 33 Raising Errors.py
@@ -8,13 +8,24 @@
 # Another example, I've make a calculator but the user is entering his wife's name instead of doing fkn calcutlation. In that case I need to raise a custom error.
 
 # The following code is a kewl example: 
-
 n = int(input("Enter a number between 0 and 9\n>>> "))
-if n<0 or n>9:
+if (n<0 or n>9):
     raise ValueError("Your number is either greater then 9 or less then 0. Try Again!")
 else:
     print(f"You entered {n}")
-    
+
+# Assert keyword:
+# For Example - we want only +ve value and if -ve than throw an error
+n = int(input(">>> "))
+if (n > 0):
+    print(n)
+else:
+    raise Exception('Enter value is -ve')
+# Alternative for the above program by using assert keyword:
+n = int(input(">>> "))
+assert(n > 0), 'Enter value is -ve' # assert(conditon),'message'
+# If the condition is false then AssertionError will occure with the given message
+
 # * QUICK QUIZ *
-# write the same following code but add a thing in which if user write "quit" then the program ends with a massage: *you quited the program*.
+# Write the same following code but add a thing in which if user write "quit" then the program ends with a massage: *you quited the program*.
  
