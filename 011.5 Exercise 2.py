5beb1b3d73d648825bf1aa5d32ce8df90f80f993
Amrit Singh <amritsingh72007@gmail.com>
2024-05-28 13:35:53 +0530

    Update 011.5 Exercise 2.py

--- 011.5 Exercise 2.py
+++ 011.5 Exercise 2.py
@@ -1,9 +1,10 @@
 # Exercise 2: Good Moring Sir
 
-# 12am - 6am  evening     00.00.00 - 06.00.00
-# 6am - 12pm  moring      06.00.00 - 12.00.00
-# 12pm - 6pm  after noon  12.00.00 - 18.00.00
-# 6pm - 12am  evening     18.00.00 - 24.00.00
+# 12-hour format    Greating       24-hour format
+# 12am - 6am        evening        00.00.00 - 06.00.00
+# 6am - 12pm        moring         06.00.00 - 12.00.00
+# 12pm - 6pm        after noon     12.00.00 - 18.00.00
+# 6pm - 12am        evening        18.00.00 - 24.00.00
 
 import time
 # This give me the current hour going on in 24-hour format:
