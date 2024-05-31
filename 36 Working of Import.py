41d1d34b78890c834831fc3b2a612b4e98ee7ee0
Amrit Singh <amritsingh72007@gmail.com>
2024-05-31 20:54:53 +0530

    Created files that no one ask for

---
+++ 36 Working of Import.py
@@ -0,0 +1,5 @@
+# One of the top most folders of the repository
+# OR just run this file:
+import webbrowser # This module is not in course. I've also picked it from stackoverflow lol.
+webbrowser.open("https://github.com/Green-Dev-il/The_Ultimate_Python/tree/12c7030506760b8c54681e905ea1e18bc9d664ba/36%20Working%20of%20Import")
+    
---
+++ 37 if __name__ == '__main__'.py
@@ -0,0 +1,4 @@
+# One of the top most folders of the repository
+# OR just run this file:
+import webbrowser # This module is not in course. I've also picked it from stackoverflow lol.
+webbrowser.open("https://github.com/Green-Dev-il/The_Ultimate_Python/tree/12c7030506760b8c54681e905ea1e18bc9d664ba/37%20if%20__name__%20%3D%3D%20'__main__'")
