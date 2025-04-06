0057e502cf0df445024e79d0e3507a73a797588e
Amrit Singh <amritsingh72007@gmail.com>
2025-04-06 13:40:00 +0530

    Update 48 Decorators.py

--- 48 Decorators.py
+++ 48 Decorators.py
@@ -34,5 +34,36 @@
 # Positional args: ('apple', 'banana')
 # Keyword args: {'fruit': 'mango', 'count': 2}
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Decorators :If we need to run a function before and after a function then we use decorators or at the rate keyword in python.
+
+# Python decorators are a powerful and versatile tool that allow you to modify the behavior of functions and methods. They are a way to extend the functionality of a function or method without modifying its source code.
+
+# A decorator is a function that takes another function as an argument and returns a new function that modifies the behavior of the original function. The new function is often referred to as a "decorated" function. The basic syntax for using a decorator is the following:
+
+# def at_the_rate(fx):
+#   def decorator(*args, **kwargs):
+#     before_fun()
+#     main_fn(*args, **kwargs)
+#     after_fun()
+#   return decorator
+
+# See this with an example:
+def greet(fx):
+  def mfx(*args, **kwargs):
+    print("Good Morning")
+    fx(*args, **kwargs)
+    print("Thanks for using this function")
+  return mfx
+
+@greet
+def hello():
+  print("Hello world")
+
+@greet
+def add(a, b):
+  print(a+b)
+  
+hello()
+add(1, 2)
